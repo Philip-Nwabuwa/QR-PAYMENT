4d0a92c8ffc741bb0b20453e623b54cf8795c839
Philip Nwabuwa <philipnwabuwa@gmail.com>
2023-02-10 12:51:51 +0100

    installed react-icons

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,21 +1,30 @@
-import QRLogo from "../assets/qr-logo.jpeg";
+import X from "../assets/x.svg";
 const Navbar = () => {
   return (
     <div className="bg-black text-white w-full h-20 flex items-center justify-center uppercase">
       <div className="flex w-[95%] justify-between items-center">
-        <div className="">QR Generator</div>
-        <ul className="flex items-center md:hidden">
+        <div className="text-2xl">QR Generator</div>
+        <ul className="md:flex items-center hidden">
           <li className="pr-2">about</li>
           <li className="pr-2">Docs</li>
           <li>contact</li>
         </ul>
-        <button>
-          <a href="/home" className="bg-white text-black">
+        <button className="md:flex hidden">
+          <a
+            href="/home"
+            className="bg-white text-black px-6 py-3 font-bold rounded-md hover:bg-gray-200 hover:underline"
+          >
             Get Started
           </a>
         </button>
 
-        <div className="md:flex hidden">close</div>
+        <button className="">
+          <img
+            className="fill-current text-white w-10 h-10"
+            src={X}
+            alt="Close Navbar"
+          />
+        </button>
       </div>
     </div>
   );
